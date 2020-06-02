5524c4fae567d291981b73e9043a9e24811b33a3
Oliver <oliver.tanevski23@gmail.com>
2020-06-02 22:12:36 +0200

    Added initial support for loading .names file with colors

--- YOLOLabeler/Form1.Designer.cs
+++ YOLOLabeler/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.labelInsertClasses = new System.Windows.Forms.Label();
+            this.colorPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.AutoScroll = true;
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.colorPanel.Controls.Add(this.labelInsertClasses);
+            this.colorPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.colorPanel.Location = new System.Drawing.Point(716, 0);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(250, 551);
+            this.colorPanel.TabIndex = 0;
+            // 
+            // labelInsertClasses
+            // 
+            this.labelInsertClasses.AutoSize = true;
+            this.labelInsertClasses.Location = new System.Drawing.Point(31, 17);
+            this.labelInsertClasses.Name = "labelInsertClasses";
+            this.labelInsertClasses.Size = new System.Drawing.Size(188, 20);
+            this.labelInsertClasses.TabIndex = 0;
+            this.labelInsertClasses.Text = "Browse for your .names file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(966, 551);
+            this.Controls.Add(this.colorPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.colorPanel.ResumeLayout(false);
+            this.colorPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Label labelInsertClasses;
     }
 }
 
--- YOLOLabeler/Form1.cs
+++ YOLOLabeler/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,83 @@ namespace YOLOLabeler
 {
     public partial class Form1 : Form
     {
+        private Button browseClasses;
+        private Random rnd = new Random();
+        private string[] arr;
+        int initTop = 80;
+        int initLeft = 20;
         public Form1()
         {
+            arr = null;
             InitializeComponent();
+            InitializeDynamic();
+          
+        }
+
+        private void InitializeDynamic() {
+            browseClasses = new Button();
+            browseClasses.Text = "Browse";
+            browseClasses.Name = "btnBrowse";
+            browseClasses.Height = 30;
+            browseClasses.Top = 40;
+            browseClasses.Left = 80;
+            browseClasses.Click += browseClassesButton_Click;
+            colorPanel.Controls.Add(browseClasses);
+
+     
+        }
+
+
+        private void browseClassesButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = ".names file (*.names)|*.names|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+    
+                var fileStream = dialog.OpenFile();
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    string content = reader.ReadToEnd();
+                    if (arr != null)
+                    {
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            colorPanel.Controls.RemoveByKey("colorLabel_" + arr[i]);
+                            colorPanel.Controls.RemoveByKey("btnColor_" + arr[i]);
+                            initTop -= 20;
+                        }
+                    }
+                    arr = content.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+                }
+
+                CreateClassesControls();
+            }
+        }
+
+        private void CreateClassesControls()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Label l = new Label();
+                l.Text = arr[i];
+                l.Name = "colorLabel_" + l.Text;
+                Button b = new Button();
+                b.Name = "btnColor_" + l.Text;
+                b.Width = 35;
+                b.Top = initTop;
+                b.Left = initLeft;
+                l.Top = initTop;
+                l.Left = initLeft + b.Width;
+                initTop += 20;
+                Color c = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                b.BackColor = c;
+
+                colorPanel.Controls.Add(b);
+                colorPanel.Controls.Add(l);
+
+            }
         }
 
     }
